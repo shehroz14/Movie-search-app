7a8d6aa7034c37ea9f754373c6c6cf95ae597326
Shehroz Basharat <shehrozbasharat7@gmail.com>
2026-07-21 21:23:59 +0500

    add selectors

---
+++ app.js
@@ -0,0 +1,15 @@
+const searchInput = document.querySelector('#searchInput');
+const searchBtn = document.querySelector('#searchBtn');
+
+const statusMsg = document.querySelector('#statusMessage');
+const loadingState = document.querySelector('#loadingState');
+const errorState  = document.querySelector('#errorState');
+const emptyState = document.querySelector('#emptyState');
+
+const img = document.querySelector('.movie-poster');
+const movieTitle = document.querySelector('.movie-title');
+const movieYear = document.querySelector('.movie-year');
+const movieRating = document.querySelector('.movie-rating');
+const movieGenre =document.querySelector('.movie-genre')
+const movieRuntime = document.querySelector('.movie-runtime');
+const movieDescription = document.querySelector('.movie-description');
