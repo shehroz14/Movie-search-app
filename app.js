4bc17851fefdfc04b24784f274749603a0fa700e
Shehroz Basharat <shehrozbasharat7@gmail.com>
2026-07-22 17:17:48 +0500

    add structure styling and functioning for movie search app

--- app.js
+++ app.js
@@ -6,10 +6,30 @@ const loadingState = document.querySelector('#loadingState');
 const errorState  = document.querySelector('#errorState');
 const emptyState = document.querySelector('#emptyState');
 
-const img = document.querySelector('.movie-poster');
+const movieImg = document.querySelector('.movie-img');
 const movieTitle = document.querySelector('.movie-title');
 const movieYear = document.querySelector('.movie-year');
 const movieRating = document.querySelector('.movie-rating');
-const movieGenre =document.querySelector('.movie-genre')
+const movieGenre = document.querySelector('.movie-genre');
 const movieRuntime = document.querySelector('.movie-runtime');
 const movieDescription = document.querySelector('.movie-description');
+
+const API_KEY  = "67f741f4"
+
+searchBtn.addEventListener('click', async ()=>{
+    let searchInputValue = searchInput.value.trim()
+        const response = await fetch(
+            `https://www.omdbapi.com/?apikey=${API_KEY}&t=${searchInputValue}`
+        );
+        const data = await response.json();
+        
+        movieImg.src = data.Poster
+        movieTitle.textContent = `Title: ${data.Title}`;
+        movieYear.textContent = `Year: ${data.Year}`;
+        movieRating.textContent = `Rating: ${data.imdbRating}⭐`;
+        movieGenre.textContent = `Genre: ${data.Genre}`;
+        movieRuntime.textContent = `Runtime: ${data.Runtime}`;
+        movieDescription.textContent = `Plot: ${data.Plot}`;
+
+})
+
